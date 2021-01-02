68b8f0f9dc0825b4f053722a19a18482b3e4c65d
kirill791 <76863554+kirill791@users.noreply.github.com>
2021-01-02 11:14:44 +0300

    Add files via upload

---
+++ clockJS.js
@@ -0,0 +1,15 @@
+setInterval(function(){
+    let date = new Date();
+
+    let format = [
+        (date.getHours()<10 ? "0"+date.getHours():date.getHours()), 
+        (date.getMinutes()<10 ? "0"+date.getMinutes():date.getMinutes()),
+        (date.getSeconds()<10 ? "0"+date.getSeconds():date.getSeconds())
+    ].join(':');
+
+   document.getElementById('clock').innerHTML = format;
+}, 900 );
+
+function ButtonClick (button) {
+    alert("Сайт остоновился!");
+}
